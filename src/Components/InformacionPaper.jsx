74aa3e7573dd6904beb301e36da506c4a5ad39ea
Francisco Cortez <franciscocortezdev@gmail.com>
2022-11-11 13:37:16 -0600

    Estadisticas del usuario

--- src/Components/InformacionPaper.jsx
+++ src/Components/InformacionPaper.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 import { Paper, Stack, Typography } from "@mui/material";
 
 export default function InformacionPaper({ DatosUsuario }) {
+  const { public_repos, followers, following } = DatosUsuario
+
   return (
-    <div>InformacionPaper</div>
+    <Paper elevation={3}>
+      <Stack>
+        <Typography>Repositorios: {public_repos}</Typography>
+        <Typography>Seguidores: {followers}</Typography>
+        <Typography>Siguiendo: {following}</Typography>
+
+      </Stack>
+    </Paper>
   )
 }
